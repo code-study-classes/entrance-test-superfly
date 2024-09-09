5c184d4233c8b910846a6635c021ca0f01e1bbb5
Vandopal <rdlo8679@gmail.com>
2024-09-10 02:52:57 +0300

    v1

--- src/fundStructure.js
+++ src/fundStructure.js
@@ -1,11 +1,20 @@
-export default function generate(hotelNumber ,number, category, status = 'free') {
-   category = ['standart' , 'lux' , 'appartments']
-   status = ['free','taken']
-  const idLogic = [];
-  let obj = {}
-  obj.hotelNumber = hotelNumber
-  obj.number = number
-  obj.category = category
-  obj.status = status
-  console.log(obj)
+import fs from 'fs';
+
+// function generate(hotelName, room, category, status = 'free') {
+//    category = ['standart' , 'lux' , 'appartments']
+//    status = ['free','taken']
+//   const idLogic = [];
+//   let obj = {}
+//   obj.hotelName = hotelName
+//   obj.room = room
+//   obj.category = category
+//   obj.status = status
+//   console.log(obj)
+// }
+
+export default class Generate {
+    constructor(name, region) {
+      this.name = name;
+      this.region = region
+    }
 }
--- src/regions.js
+++ src/regions.js
@@ -1,4 +1,26 @@
-const regions = (regionsPath) => {
-  return regionsPath
+// const regions = (regionsPath) => {
+//   return regionsPath
+// }
+// export default regions
+
+export default class Country {
+  constructor () {
+    this.country = [];
+  }
+
+  getCountry () {
+    return this.country
+  }
+
+  setCountry (addCountry) {
+    const items = this.getCountry()
+    items.push(addCountry)
+  }
 }
-export default regions
+
+const myCountry = new Country();
+myCountry.setCountry({ region: 'Moscow'});
+myCountry.setCountry({ region : "Saint-Peterburg"});
+myCountry.setCountry({ region : "Stockholm"});
+myCountry.setCountry({ region : "Abakan"});
+console.log(myCountry.getCountry());
